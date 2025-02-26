b15af97605016e74bd24c96af9845b85473bb82c
Otoolorin <volasupo5@gmail.com>
2025-02-26 23:43:52 +0100

    Get references to calc elements

---
+++ script.js
@@ -0,0 +1,4 @@
+// references to interactive calculator elements
+const calcBtns = document.querySelector('.buttons');
+const operationDisplay = document.querySelector('.operation');
+const resultDisplay = operationDisplay.nextElementSibling;
