62faa44c6bb95360c91c43ef158762d9705371cd
Otoolorin <volasupo5@gmail.com>
2025-02-27 00:08:20 +0100

    Replace the object literal with an array
    Since we can directly access the ext content in each button,
    which happens to be their respective value in the object, It
    is better to save their ID values in an array instead.

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const calcBtns = document.querySelector('.buttons');
 const operationDisplay = document.querySelector('.operation');
 const resultDisplay = operationDisplay.nextElementSibling;
 
-// map all numeral buttons to their values
-const numerals = {
-    one: 1, two: 2,
-    three: 3, four: 4,
-    five: 5, six: 6,
-    seven: 7, eight: 8,
-    nine: 9, zero: 0,
-    dot: '.', // decimal point
-    negation: '-' // negation sign
-}
+// All digits, decimal point, and negation signs ID Values 
+const idValues = [
+    'one', 'two',
+    'three', 'four',
+    'five', 'six',
+    'seven', 'eight',
+    'nine', 'zero',
+    'dot', // decimal point
+    'negation' // negation sign
+]
 
 // operands to store user inputs
 let firstOperand = '';
