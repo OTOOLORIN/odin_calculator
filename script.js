85cdcace12fb59241822c026dbc08c5d9d6f3c8b
Otoolorin <volasupo5@gmail.com>
2025-02-26 23:44:42 +0100

    map number buttons to their values

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 const calcBtns = document.querySelector('.buttons');
 const operationDisplay = document.querySelector('.operation');
 const resultDisplay = operationDisplay.nextElementSibling;
+
+// map all numeral buttons to their values
+const numerals = {
+    one: 1, two: 2,
+    three: 3, four: 4,
+    five: 5, six: 6,
+    seven: 7, eight: 8,
+    nine: 9, zero: 0,
+    dot: '.', // decimal point
+    sign: '-' // negation sign
+}
