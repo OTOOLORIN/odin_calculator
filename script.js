4c5e5a58983743071319a8829e3923abd0a1d0f3
Otoolorin <volasupo5@gmail.com>
2025-03-08 00:44:12 +0100

    Get references for useful DOM elements

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-// references to interactive calculator elements
-const calcBtns = document.querySelector('.buttons');
-const operationDisplay = document.querySelector('.operation');
-const resultDisplay = operationDisplay.nextElementSibling;
-
-// All digits, decimal point, and negation signs ID Values 
-const idValues = [
-    'one', 'two',
-    'three', 'four',
-    'five', 'six',
-    'seven', 'eight',
-    'nine', 'zero',
-    'dot', // decimal point
-    'negation' // negation sign
-]
-
-// operands to store user inputs
-let firstOperand = '';
-let secondOperands = ''
-let operationResult; // result of the operation
+// references to DOM elements
+const firstOperandDisplay = document.querySelector('.first-operand');
+const secondOperandDisplay = document.querySelector('.second-operand');
+const operatorBox = document.querySelector('.operator');
+const addBtn = document.querySelector('.add');
+const subBtn = document.querySelector('.subtract');
+const divBtn = document.querySelector('.divide');
+const mulBtn = document.querySelector('.multiply');
+const percentBtn = document.querySelector('.percent');
+const resultDisplay = document.querySelector('.result');
+const signBtn = document.querySelector('#negation');
+const decimalBtn = document.querySelector('#dot');
+const allBtns = document.querySelector('.buttons');
+const clearBtn = document.querySelector('.clear');
+const equalBtn = document.querySelector('#equal');
+const deleteBtn = document.querySelector('#delete');
